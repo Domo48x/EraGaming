e57dc22db4db8e4be80550540392c03a02506989
Carlos Garza <carlos.garza2568@gmail.com>
2020-11-23 17:32:28 -0600

    Test commit 2

--- MatlabCodes/PlantillaScara.m
+++ MatlabCodes/PlantillaScara.m
@@ -2,8 +2,8 @@ clear all
 close all
 clc
 
-L1=Link([pi/2 0 2 0 0 0]); %Eslabón 1
-L2=Link([0 2 0 0 0 0]); %Eslabón 0
+L1=Link([pi/4 0 4 0 0 0]); %Eslabón 1
+L2=Link([0 4 0 0 0 0]); %Eslabón 0
         
 scara = SerialLink([L1 L2],'name','ROBOT 3 ARTICULACIONES');
 scara.plot([0 0])
